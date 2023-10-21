167be495b7105528c208faf2e2ad1f5a686fcc2f
lefthand67 <lefthand67@yandex.ru>
2023-10-22 00:26:31 +0500

    week 5 started

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Welcome! 
+﻿Welcome! 
 
-Here are my solutions of the Harvard's CS50 course's problems (./Solutions). Solutions are written primarily in java language, but some are in Python and C. 
+Here are my solutions of the Harvard's CS50 course's problems (./Solutions). Some portion of solutions (first weeks) were wtitten in java because I did not consider C as a language I need to learn, but when there lectures on memory started I had to realize its importance. 
 
 Descriptions of the problems are stored in the JupyterNB file (./HarvardCS50_problems.ipynb). ./data directory contains supplementary files for problems description (like images, gifs, etc.).
--- Solutions/draft.c
+++ Solutions/draft.c
@@ -1,22 +1,28 @@
+#include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-// #include <cs50.h>
+
+typedef struct person
+{
+    struct person *parents[2];
+    char alleles[2];
+}
+person;
 
 int main(void)
 {
-	int Gx[3][3] = {
-		{-1, 0, 1},
-		{-2, 0, 2},
-		{-1, 0, 1}
-	};
-	
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			printf("%i ", Gx[i][j]);
-		}
-	printf("\n");
-	}
-}
+	person *p = malloc(sizeof(person));
+	p->parents[0] = NULL;
+	p->parents[1] = NULL;
+	p->alleles[0] = 'a';
 
+    char allele = p->alleles[0];
+	printf("hello: %c\n", allele);
+
+
+	person *s = malloc(sizeof(person));
+	s->parents[0] = p;
+	printf("again: %c\n", s->parents[0]->alleles[0]);
+}
